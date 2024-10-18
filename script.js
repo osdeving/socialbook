a2c1032120c58256c9a55ad387c548ac7715e526
Willams Sousa <osdeving@ghmail.com>
2024-10-18 04:07:09 -0300

    feat: adicionar tema no localstorage

--- script.js
+++ script.js
@@ -11,4 +11,22 @@ const darkBtn = document.getElementById("dark-btn");
 darkBtn.addEventListener("click", () => {
     darkBtn.classList.toggle("dark-btn-on");
     body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") === "light") {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+
 });
+
+if (localStorage.getItem("theme") === "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    body.classList.remove("dark-theme");
+} else if(localStorage.getItem("theme") === "dark") {
+    darkBtn.classList.add("dark-btn-on");
+    body.classList.add("dark-theme");
+} else {
+    localStorage.setItem("theme", "light");
+}
+
